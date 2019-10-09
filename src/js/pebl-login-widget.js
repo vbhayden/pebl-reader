40501e2541f3555b26aefde4b27778d266facc34
Mile Divovic <md2950@nyu.edu>
2019-10-09 11:06:27 -0700

    remove arrow function in login widget

--- src/js/pebl-login-widget.js
+++ src/js/pebl-login-widget.js
@@ -259,8 +259,9 @@ window.Lightbox = {
     },
 
     uuidv4: function () {
-        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, function(c) {
             (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        }
         );
     },
     buildLinkedInAuthCodeUrl: function (scope, clientId, redirectUri, state) {
