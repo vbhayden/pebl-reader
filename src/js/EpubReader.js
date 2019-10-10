5a98ed3ddb3d99fb0be5f97eb6f93ad88fb75c86
Mile Divovic <md2950@nyu.edu>
2019-10-10 11:39:17 -0700

    add event hooks for text selection

--- src/js/EpubReader.js
+++ src/js/EpubReader.js
@@ -2358,6 +2358,44 @@ define([
                     $('#reading-area').removeClass("contentFocus");
                 });
 
+                readium.reader.addIFrameEventListener('mouseup', function(e) {
+                    var text = "";
+                    if (e.view) {
+                        if (typeof e.view.getSelection != "undefined") {
+                            text = e.view.getSelection().toString();
+                        } else if (typeof e.view.document.selection != "undefined" && e.view.document.selection.type == "Text") {
+                            text = e.view.document.selection.createRange().text;
+                        }   
+                    }
+                    
+                    if (text.length > 0) {
+                        console.log('Text is selected');
+                        // Show the highlight button
+                    } else {
+                        console.log('No text selected');
+                        // Hide the highlight button
+                    }
+                });
+
+                readium.reader.addIFrameEventListener('touchend', function(e) {
+                    var text = "";
+                    if (e.view) {
+                        if (typeof e.view.getSelection != "undefined") {
+                            text = e.view.getSelection().toString();
+                        } else if (typeof e.view.document.selection != "undefined" && e.view.document.selection.type == "Text") {
+                            text = e.view.document.selection.createRange().text;
+                        }   
+                    }
+                    
+                    if (text.length > 0) {
+                        console.log('Text is selected');
+                        // Show the highlight button
+                    } else {
+                        console.log('No text selected');
+                        // Hide the highlight button
+                    }
+                });
+
                 SettingsDialog.initDialog(readium.reader);
 
                 $('#settings-dialog').on('hidden.bs.modal', function() {
