f1fcfa8fb631bc8f2cde8117b898d312b50d1360
Mile Divovic <md2950@nyu.edu>
2020-02-14 14:59:44 -0800

    add apache license

--- src/js/gestures.js
+++ src/js/gestures.js
@@ -1,3 +1,17 @@
+/*
+Copyright 2020 Eduworks Corporation
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+Modified in commit 994a17c6cf0b38051b98f4b9a126a18f28fd0e7e
+*/
+
 //  Copyright (c) 2014 Readium Foundation and/or its licensees. All rights reserved.
 //  
 //  Redistribution and use in source and binary forms, with or without modification, 
