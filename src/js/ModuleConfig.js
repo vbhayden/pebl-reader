5f64c30365341bc2b34514090af4b28f7935da2f
Mile Divovic <md2950@nyu.edu>
2020-04-22 20:34:36 -0700

    change default configs

--- src/js/ModuleConfig.js
+++ src/js/ModuleConfig.js
@@ -18,9 +18,9 @@ define(['module'], function(module) {
 
             'workerUrl': config.workerUrl || "scripts/readium-js-viewer_CLOUDAPP-WORKER.js",
 
-            'annotationCSSUrl': config.annotationCSSUrl || "css/annotations.css",
-            'mathJaxUrl': config.mathJaxUrl || "scripts/mathjax/MathJax.js",
-            'jsLibRoot': config.jsLibRoot || "scripts/zip/",
+            'annotationCSSUrl': config.annotationCSSUrl || "/ANNOTATIONS.css",
+            'mathJaxUrl': config.mathJaxUrl || "/MATHJAX.js",
+            'jsLibRoot': config.jsLibRoot || "/ZIPJS/",
 
             //Fonts is a list of font objects. 
             'fonts': config.fonts || [],
