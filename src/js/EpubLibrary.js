6f4ac5e5267974ae59d5c03fbcacaff85df1037f
Mile Divovic <md2950@nyu.edu>
2020-08-02 16:32:50 -0700

    fix typo

--- src/js/EpubLibrary.js
+++ src/js/EpubLibrary.js
@@ -1246,7 +1246,7 @@ define([
                    var quizes = Object.keys(e.detail).map(function(x) {
                      return e.detail[x];
                    }).sort(function(a, b) {
-                     return a.count - b.count;
+                     return b.totalCount - a.totalCount;
                    });
 
                    for (var i = 0; i < quizes.length; i++) {
