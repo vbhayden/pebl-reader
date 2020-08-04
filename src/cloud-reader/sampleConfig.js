335fa6cf561761035c478b82c209934171060640
Aaron Veden <aaron.veden@eduworks.com>
2020-08-04 14:12:27 -0700

    adding docker auto build

---
+++ src/cloud-reader/sampleConfig.js
@@ -0,0 +1,22 @@
+/*
+Copyright 2020 Eduworks Corporation
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+window.PeBLConfig = {}
+window.PeBLConfig.PeBLServicesURL = '__HTTP_SERVICES_ENDPOINT__';
+window.PeBLConfig.PeBLServicesWSURL = '__WS_SERVICES_ENDPOINT__';
+window.PeBLConfig.useOpenID = true;
+window.PeBLConfig.favicon = 'images/usmc-favicon-16x16.png';
+window.PeBLConfig.loginImage = 'images/usmc-android-chrome-192x192.png';
+window.PeBLConfig.appleTouchIcon = 'images/usmc-apple-touch-icon.png';
+window.PeBLConfig.userList = ['Learner', 'Learner1', 'Learner2', 'Learner3', 'Learner5', 'Learner7'];
+window.DEBUGGING = false;
